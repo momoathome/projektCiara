1cd4c5c9abeb7f249a023fdd7b9f46b6ac6b5719
SnowOwl <mknoll1901@gmail.com>
2022-01-18 20:16:16 +0100

    finished Overview

--- src/js/pages/overview/overview.js
+++ src/js/pages/overview/overview.js
@@ -1,18 +1,20 @@
-import {anzahlCheck} from '../../helper/checkFunction.js'
-import {writeValueToTable} from '../structures/structureUpdate.js'
-import dbData from '../../helper/getData.js'
-import config from '../../config.js'
+const structure = JSON.parse(localStorage.getItem('structures'))
+const units = JSON.parse(localStorage.getItem('units'))
+const ressource = JSON.parse(localStorage.getItem('ressources'))
 
-const createUnitModule = (data) => {
+const createUnitModule = () => {
   // tableBody
   function createTableBody() {
     const tbody = document.querySelector('.table__body--unit')
-    data.units.forEach((unit, i) => {
+    units.forEach((unit) => {
       const tr = document.createElement('tr')
       const combat = unit.combat
         .toString()
         .replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.')
       const cargo = unit.cargo
+        .toString()
+        .replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.')
+      const quantity = unit.quantity
         .toString()
         .replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.')
       const tableData =
@@ -21,33 +23,37 @@ const createUnitModule = (data) => {
           <td>${unit.name}</td>
           <td>${combat}</td>
           <td>${cargo}</td>
-          <td id="unit_cargo_${i}"></td>
+          <td>${quantity}</td>
         `
       tr.innerHTML = tableData
       tbody.appendChild(tr)
     })
   }
   createTableBody()
-  anzahlCheck()
 }
 
-const createStationModule = (data) => {
+const createStationModule = () => {
   function createTableBody() {
     const tbody = document.querySelector('.table__body--structure')
 
-    data.structures.forEach((structure, i) => {
+    structure.forEach((structure) => {
       const row = document.createElement('tr')
       const energie = structure.energie
         .toString()
         .replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.')
-      // const credits = genData[i].cost.credits.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.')
+      const cost = structure.cost.credits
+        .toString()
+        .replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.')
+      const level = structure.level
+        .toString()
+        .replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.')
       const tableData =
         /* html */
         `
           <td>${structure.name}</td>
           <td>${energie}<span class="font">E</span></td>
-          <td class="kostenValue"><span class="font">C</span></td>
-          <td class="stufenValue stufe_${i}"></td>
+          <td>${cost}<span class="font">C</span></td>
+          <td>${level}</td>
         `
       row.innerHTML = tableData
       tbody.appendChild(row)
@@ -55,22 +61,27 @@ const createStationModule = (data) => {
   }
 
   createTableBody()
-  writeValueToTable()
 }
 
-function createMarketModule(data) {
+function createMarketModule() {
   function createTableBody() {
     const tableBody = document.querySelector('.table__body--market')
-    data.ressources.forEach((roh, i) => {
+    ressource.forEach((roh) => {
       const row = document.createElement('tr')
+      const stock = roh.stock
+        .toString()
+        .replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.')
+      const quantity = roh.quantity
+        .toString()
+        .replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.')
 
       const tableData =
         /* html */
         `
         <td>${roh.name}</td>
-        <td class="stockTd"></td>
-        <td class="valueTd"></td>
-        <td class="maxValue_${i}"></td>
+        <td>${stock}t</td>
+        <td>${roh.cost}<span class="font">C</span></td>
+        <td>${quantity}</td>
         `
       row.innerHTML = tableData
       tableBody.appendChild(row)
@@ -78,56 +89,8 @@ function createMarketModule(data) {
   }
 
   createTableBody()
-  updateMarketModuleData(data)
 }
 
-function updateMarketModuleData(data) {
-  const maxRoh = []
-  const currentStock = []
-  const varFaktor = []
-  const price = []
-
-  function maxRessource() {
-    data.ressources.forEach((res, i) => {
-      maxRoh[i] = localStorage.getItem(`roh_${i}`)
-      const maxValueSpan = document.querySelector(`.maxValue_${i}`)
-      maxValueSpan.innerText = maxRoh[i]
-    })
-    getMarketData()
-    updateStock()
-    updateValue()
-  }
-  function getMarketData() {
-    data.ressources.forEach((res, i) => {
-      currentStock[i] = parseInt(localStorage.getItem(`stock_${i}`))
-      varFaktor[i] = parseFloat(localStorage.getItem(`varFaktor_${i}`))
-      price[i] = Math.round(
-        config.basePrice[i] + (varFaktor[i] - config.varFaktor)
-      )
-    })
-  }
-  function updateStock() {
-    const stockTd = document.querySelectorAll('.stockTd')
-    stockTd.forEach((element, i) => {
-      const stock = currentStock[i]
-        .toString()
-        .replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.')
-      element.innerText = `${stock}t`
-    })
-  }
-
-  function updateValue() {
-    const valueTd = document.querySelectorAll('.valueTd')
-    valueTd.forEach((element, i) => {
-      const value = price[i]
-        .toString()
-        .replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.')
-      element.innerHTML = `${value}<span class="font">C</span>`
-    })
-  }
-  maxRessource()
-}
-
-createUnitModule(dbData)
-createStationModule(dbData)
-createMarketModule(dbData)
+createUnitModule()
+createStationModule()
+createMarketModule()
