fdcd56f26cc8a705714661c7af3e97ca3d09aa28
SnowOwl <mknoll1901@gmail.com>
2022-01-26 14:19:17 +0100

    added active class in Navigation

--- src/js/common/navigation.js
+++ src/js/common/navigation.js
@@ -3,6 +3,7 @@ const gameName = 'Projekt Ciara'
 const createNavigation = () => {
   const nav = document.querySelector('.nav')
   const navDiv = document.createElement('div')
+  navDiv.classList.add('nav__flex-container')
 
   const navLogo =
     /* html */
@@ -23,9 +24,22 @@ const createNavigation = () => {
     <li><a href="#">#Ranking#</a></li>
     </ul>
     `
-  navDiv.classList.add('nav__flex-container')
   navDiv.innerHTML = navLogo + navElements
   nav.appendChild(navDiv)
+
+  activeElement()
+}
+
+const activeElement = () => {
+  const path = window.location.pathname.slice(11)
+  const navList = document.querySelector('.nav__list')
+
+  for (const item of navList.children) {
+    const linkHref = item.children[0].attributes[0].value
+    if (path === linkHref.slice(2)) {
+      item.children[0].classList.add('active')
+    }
+  }
 }
 
 export {createNavigation}
